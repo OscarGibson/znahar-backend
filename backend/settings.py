50c8be7874c09e071ec5301315675bf56170e69e
Ostap Hyba <oneostap@gmail.com>
2019-10-25 00:16:09 +0300

    sve()

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Third-party
     'corsheaders',
     'rest_framework',
+    'cloudinary',
     # Custom
     'base',
     'customUser',
---
+++ base/__init__.py
@@ -0,0 +1,8 @@
+import cloudinary
+
+
+cloudinary.config( 
+  cloud_name = "starter", 
+  api_key = "124275534473187", 
+  api_secret = "cmdsIpgZADP1sxjS0r1SCfhmHWI" 
+)
