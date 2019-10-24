4b10285f34da625f443612a7540d0887b9c610f1
Ostap Hyba <oneostap@gmail.com>
2019-10-25 00:38:42 +0300

    save()

--- base/__init__.py
+++ base/__init__.py
@@ -2,7 +2,7 @@ import cloudinary
 
 
 cloudinary.config( 
-  cloud_name = "starter", 
+  cloud_name = "hswukmday", 
   api_key = "124275534473187", 
   api_secret = "cmdsIpgZADP1sxjS0r1SCfhmHWI" 
 )
