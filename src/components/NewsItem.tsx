9a2ff388135da1347eba3cd5f26c562bc060ddd9
Ostap Hyba <oneostap@gmail.com>
2019-11-18 02:25:21 +0200

    fixed new block styles

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -3,17 +3,16 @@ import { connect } from 'react-redux'
 import { INewsItem } from '../types'
 
 const NewsItem = ({
-    image, title, 
+    image, title, id,
     content_short, date,
 }:INewsItem) => {
     return (
-        <div className="NewsItem col-md-4 col-sm-6 col-xs-12">
+        <div className="NewsItem col-md-4 col-sm-6 col-xs-12" onClick={() => {window.location.href = `/news/${id}`}}>
             <img className="image" src={image} alt="photoUrl"/>
             <h6 className="title">{title}</h6>
             <p className="description">{content_short.slice(0, 75)}...</p>
             <div className="bottomBlock">
                 <p className="date">{date}</p>
-                {/* <p className="views">{views}</p> */}
             </div>
         </div>
     )
