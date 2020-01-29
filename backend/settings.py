86200af6468fc7127f0e27a5b540bde81167c53d
OscarGibson <oneostap@gmail.com>
2020-01-29 18:58:36 +0200

    removed feedback form

--- backend/settings.py
+++ backend/settings.py
@@ -213,4 +213,4 @@ GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'backend', 'znahar-ed9e0d984345.json')
 )
 
-SENDGRID_API_KEY='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'
+SENDGRID_API_KEY='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'
--- src/components/FooterBlock.tsx
+++ src/components/FooterBlock.tsx
@@ -32,16 +32,13 @@ const FooterBlock = ({
                     <FooterBackForm {...backForm}/>
                 </div> */}
 
-                <div className="col-md-6 col-sm-12">
+                {/* <div className="col-md-6 col-sm-12">
                     <div className="row">
-                        {/* <div className="col-md-5  col-sm-12">
-                            <FooterSocial {...socialList}/>
-                        </div> */}
                         <div className="col-md-12  col-sm-12">
                             <FooterBackForm {...backForm}/>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
 
             </div>
